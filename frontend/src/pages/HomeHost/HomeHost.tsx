5f150e3d5473be81000fa3a97ef5a106e94c1025
rodrisebastian <rodrigoriquelme2198@gmail.com>
2026-07-31 23:39:32 -0300

    feat: add HomeHost CTA section

--- frontend/src/pages/HomeHost/HomeHost.tsx
+++ frontend/src/pages/HomeHost/HomeHost.tsx
@@ -1,9 +1,11 @@
 import HeroHost from "./components/HeroHost";
+import CtaHost from "./components/CtaHost";
 
 const HomeHost = () => {
   return (
     <div>
       <HeroHost />
+      <CtaHost />
     </div>
   );
 };
