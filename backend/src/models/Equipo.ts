febb9b22d93b6a5af208ae188a19f904d4b6ccd7
Marco Carreira <marco.carreira.22n@gmail.com>
2026-08-05 00:27:39 -0300

    creacion del modelo de equipo

--- backend/src/models/Equipo.ts
+++ backend/src/models/Equipo.ts
@@ -1,1 +1,77 @@
+import { Schema, model, Document, Types } from "mongoose";
 
+// Interfaz auxiliar para la subdocumentación de historial
+export interface HistorialPartido {
+  idPartido: Types.ObjectId;
+  fecha: String;
+  resultado: String;
+}
+
+export enum Sexo{
+  MASCULINO = 'MASCULINO',
+  FEMENINO = ' FEMENINO',
+  MIXTO = 'MIXTO',
+}
+
+// Interfaz del documento
+export interface Equipo extends Document {
+  nombre: String;
+  fotoPerfil: String;
+  descripcion: String;
+  puntos: Number;
+  ubicacion: String;
+  sexo: String;
+  historial: HistorialPartido[];
+}
+
+// Schema de mongoose
+const equipoSchema = new Schema<Equipo>(
+  {
+    nombre: {
+      type: String,
+      required: [true, "El nombre del equipo es obligatorio"],
+      trim: true,
+    },
+    fotoPerfil: {
+      type: String,
+      default: "",
+    },
+    descripcion: {
+      type: String,
+      default: "",
+    },
+    puntos: {
+      type: Number,
+      default: 0,
+    },
+    ubicacion: {
+      type: String,
+      required: [true, "La ubicación del equipo es obligatoria"],
+    },
+    sexo: {
+      type: String,
+      enum: Object.values(Sexo),
+      default: '',
+      required: [true, "El sexo del equipo es obligatorio"],
+    },
+    historial: [
+      {
+        idPartido: {
+          type: Schema.Types.ObjectId,
+          ref: "Partido",
+        },
+        fecha: {
+          type: String,
+          required: true,
+        },
+        resultado: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+export const Equipo = model<Equipo>("Equipo", equipoSchema);
