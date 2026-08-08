fe414acbef05c8a2c1d8a08cba79ba6a4d31122e
rodrisebastian <rodrigoriquelme2198@gmail.com>
2026-08-08 11:26:54 -0300

    feat: add tarjetas de estadísticas del Dashboard

--- frontend/src/pages/PerfilPlayer/Dashboard.tsx
+++ frontend/src/pages/PerfilPlayer/Dashboard.tsx
@@ -1,10 +1,12 @@
 import DashboardBienvenida from "./components/DashboardBienvenida";
+import DashboardStats from "./components/DashboardStats";
 import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
     <div className="player-dashboard">
       <DashboardBienvenida />
+      <DashboardStats />
     </div>
   );
 };
---
+++ frontend/src/pages/PerfilPlayer/components/DashboardStats.tsx
@@ -0,0 +1,25 @@
+import StatCardPlayer from "./StatCardPlayer";
+import { BallIcon, ClockIcon } from "./icons";
+import { datosUsuario } from "../playerData";
+import "./DashboardStats.css";
+
+const DashboardStats = () => {
+  return (
+    <div className="player-dashboard__stats">
+      <StatCardPlayer label="Partidos jugados" value={String(datosUsuario.partidosJugados)} icon={<BallIcon />} />
+      <StatCardPlayer label="Horas jugadas" value={`${datosUsuario.horasJugadas} h`} icon={<ClockIcon />} />
+      <StatCardPlayer
+        label="Equipos"
+        value={`${datosUsuario.equiposCantidad} / ${datosUsuario.equiposMax}`}
+        icon={<img src={`${import.meta.env.BASE_URL}assets/icons/equipos.svg`} alt="" />}
+      />
+      <StatCardPlayer
+        label="Complejos visitados"
+        value={String(datosUsuario.complejosVisitados)}
+        icon={<img src={`${import.meta.env.BASE_URL}assets/icons/canchas.svg`} alt="" />}
+      />
+    </div>
+  );
+};
+
+export default DashboardStats;
